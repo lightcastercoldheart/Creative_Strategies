a16761570601d26f85398dbd79892a7c92731aa6
Melvin Alvarez <alvarezmelvin@me.com>
2016-09-27 15:07:04 -0700

    092316
    transitioning to php instead of using javascript to avoid using query.
    Still trying to figure out how to do random number generator then
    append number to card database… - 3 more days

---
+++ randomizer.js
@@ -0,0 +1,11 @@
+var randomNumber = Math.floor((Math.random() * 100) + 1);
+console.log(randomNumber);
+
+
+var strategyNumber = Math.floor((Math.random() * 100) + 1);
+console.log(strategyNumber); //test to console if it works
+// find element with id="creativeStrategy"
+// var creativeStrategy = document.getElementbyId("creativeStrategy");
+$.getJSON('strategies.json', function() {
+
+});
